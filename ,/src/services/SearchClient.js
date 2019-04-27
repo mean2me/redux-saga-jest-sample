00b5c8390a739c34f61c18e3965d319ef42de57d
mean2me <emanuele.colonnelli@mean2.me>
2019-04-27 14:45:11 +0700

    repo cleanup

--- ,/src/services/SearchClient.js
+++
@@ -1,8 +0,0 @@
-class SearchClient {
-  static async search(params) {
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    return { result: {itineraries: [{value:"success"}], query: {}, summary: {} }};
-  }
-}
-
-export default SearchClient;
